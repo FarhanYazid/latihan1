3e73ef389beb83714a3eedb81accd75ca56cd2d0
FarhanYazid <79645652+FarhanYazid@users.noreply.github.com>
2021-02-25 22:03:22 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <tittle></tittle>
+    <meta charset="utf-8">
+    <meta name="viewport" content="with=device-width, initial-scale=1">
+    <link rel="stylesheet" href="asset/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    
+</head>
+
+<body style="background-color: blue ;">
+    <div class="jumbotron text-center">
+        <h1>Sistem Informasi Kartu Prakerja</h1>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-xs-12 colom1">
+                <h3>Kartu Prakerja</h3>
+                <img src="kartu.jpg"style="width:300px;height:150px;">
+                <p class="justify">
+                Kartu Prakerja adalah sebuah kartu yang digalangkan dalam rangka program pelatihan
+                dan pembinaan warga negara Indonesia yang belum memiliki keterampilan. Kartu tersebut 
+                dipromosikan oleh Joko Widodo pada masa kampanye Pemilihan umum Presiden Indonesia 2019, 
+                bersama dengan KIP Kuliah dan Kartu sembako murah.
+                </p>
+            </div>
+            <div class="col-lg-6 col-md-6 col-xs-12 sidebar" >
+                <h3>Syarat dan Alur Menjadi Peserta Kartu Prakerja</h3>
+                <p> 
+                    <ol>1.Pekerja yang ingin meningkatkan keterampilan</ol>
+                    <ol>   2.Tidak sedang menjalani pendidikan formal</ol>
+                    <ol>   3.Pekerja formal atau informal</ol>
+                    <ol>   4.WNI berusia 18 tahun ke atas</ol>
+                    <ol>   5.Pelaku usaha mikro dan kecil</ol>
+                    <ol>   6.Korban PHK</ol>
+                 </p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
